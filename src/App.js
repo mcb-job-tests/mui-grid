36d1cacc9f82dcd3506c788a17a9893f3d1d58c9
Mike Bell <mike.bell@basement.crowd.com>
2019-05-01 13:47:16 +0100

    1st alpha commit

--- src/App.js
+++ src/App.js
@@ -18,44 +18,87 @@ function App() {
   return (
     <div className="App">
       <MuiThemeProvider theme={theme}>
+        <div style={{margin:8}}>
+          <Grid container spacing={8}>
 
-        <Grid container xs={12} justify={'flex-start'} alignItems={'flex-start'}>
-          {/*First Column*/}
-            <Grid container xs={12} md={6} direction={"column"}>
-              <Grid container xs >
-                <Grid item xs={12} className='box setting-up'>
-                  <div ><p>Setting up</p></div>
-                </Grid>
-                <Grid item xs={12} className='box setting-up'>
-                  <div ><p>Staff</p></div>
-                </Grid>
+            {/*First Column*/}
+            <Grid item xs={12} md={6}>
+              <Grid container spacing={8}>
+
+                <div className={'item-wrapper'}>
+                  <Grid item xs={12} className='box setting-up'>
+                    <div><p>Setting up</p></div>
+                  </Grid>
+                </div>
+
+                <div className={'item-wrapper'}>
+                  <Grid item xs={12} className='box staff'>
+                    <div><p>Staff</p></div>
+                  </Grid>
+                </div>
+
+                <div className={'item-wrapper'}>
+                  <Grid item xs={12} className='box image-2'>
+                    <div><p>IMAGE 2</p></div>
+                  </Grid>
+                </div>
               </Grid>
             </Grid>
 
-          <Grid container xs={12} md={6} direction={"column"} >
-            <Grid container xs={12} >
-                <Grid item xs={12} className='box operations' >
-                  <div ><p>Operations</p></div>
+            {/*Second Column*/}
+            <Grid item xs={12} md={6}>
+              <Grid container spacing={8}>
+
+                <div className={'item-wrapper'}>
+                  <Grid item xs={12} className='box operations' >
+                    <div ><p>Operations</p></div>
+                  </Grid>
+                </div>
+
+                <div className={'item-wrapper'}>
+                  <Grid container>
+                    <Grid item xs={12} md={6} className='box image-1'>
+                        <div ><p>IMAGE 1</p></div>
+                    </Grid>
+
+                    <Grid item xs={12} md={6} className='box selling-a-business'>
+                      <div ><p>Selling a Business</p></div>
+                    </Grid>
+                  </Grid>
+                </div>
+
+                <div className={'item-wrapper'}>
+                  <Grid item xs={12} className='box dealing-with-disputes' >
+                    <div ><p>Dealing with disputes</p></div>
+                  </Grid>
+                </div>
+
+                <div className={'item-wrapper'}>
+                  <Grid item xs={12} className='box trading' >
+                    <div><p>Trading</p></div>
+                  </Grid>
+                </div>
+
+              </Grid>
+            </Grid>
+
+            {/*Bottom Row*/}
+            <Grid item xs={12} className='bottom-row' >
+              <Grid container>
+                <Grid item xs={12} md={4} className='box company-admin'>
+                  <div><p>Company admin</p></div>
                 </Grid>
-                <Grid item xs={12} className='box selling-a-business'>
-                  <div ><p>Selling a Business</p></div>
+                <Grid item xs={12} md={4} className='box directors'>
+                  <div><p>Directors</p></div>
                 </Grid>
+                <Grid item xs={12} md={4} className='box finance'>
+                  <div><p>Finance</p></div>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
-          {/*Second Column*/}
-          {/*<Grid container xs={12} md={6} direction={"column"} className={"border"}>
-            <Grid item container direction={"row"} xs={12} className={"border"}>
-              <Grid item  xs={12} className='box operations border'>
-                <div ><p>Operations</p></div>
-              </Grid>
-              <Grid item direction={"row"} xs={12} className={"box selling-a-business border"}>
-                <div><p>Selling a business</p></div>
-              </Grid>
-            </Grid>
-          </Grid>*/}
 
-
+        </div>
       </MuiThemeProvider>
     </div>
   );
