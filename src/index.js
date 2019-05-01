6b4a23a4b61cf6c01e7471152f996eb686fb7a47
Mike Bell <mike.bell@basement.crowd.com>
2019-05-01 16:57:30 +0100

    ie-fix-v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// These must be the first lines in src/index.js
+import "@babel/polyfill";
+import 'react-app-polyfill/ie11'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
